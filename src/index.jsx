5e20bb48b09af913ad598cd93547970d275974ea
jmgerber <jmgerber57@gmail.com>
2023-03-17 23:43:43 +0100

    Ajout du style du root

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import Footer from './components/Footer'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700;800&display=swap');
   * {
     margin: 0;
     padding: 0;
@@ -17,6 +16,11 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
     font-family: 'Montserrat';
+  }
+  #root {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
   }
 `
 
