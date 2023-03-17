5e65bd31e9c25d7eeb69bd660b4f5e318aef970f
jmgerber <jmgerber57@gmail.com>
2023-03-17 23:43:16 +0100

    Ajout d'une route rent

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Error from '../components/Error'
+import Rent from '../pages/Rent'
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/rent">
+        <Route path=":id" element={<Rent />} />
+      </Route>
       <Route path="*" element={<Error />} />
     </Routes>
   )
